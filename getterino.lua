5ae18997d2833de69c663ea9b85a751e92f4a9f3
= <jochembrouwer@telfort.nl>
2016-03-13 23:42:14 +0100

    works now

--- getterino.lua
+++ getterino.lua
@@ -1,29 +1,47 @@
 local URLS = require('legeturls');
 local CHECK = require('lecheckurl');
 
+local intr_file = io.open('intr_log')
+
 local dennis = {}
 local jobro  = {}
 local both = {}
 
 --URLS = {"http://web.archive.org/web/20080323111510/http://www.rctholland.nl/forum/viewtopic/20964"}
+--URLS = {URLS[1]}
+
+local nUrl = #URLS;
 
 for _, url in pairs(URLS) do
+	local urstr = _.."/"..nUrl;
+	local r = _/nUrl;
+	local r_round = r*100;
+	local lepercent = math.floor(r_round*100 + 0.5)/100;
+	print(urstr.."   ("..lepercent.."%)")
 	CHECK(url, function(buff)
-		print(buff)
+--		print(buff)
 		local dennis_yes, jobro_yes
 		if buff:match("Dennisvdz") then
 			table.insert(dennis, url)
+			intr_file:write("Dennis: \n");
+			intr_file:write(url.."\n")
+			intr_file:flush()
 			dennis_yes=true
+			print("DENNIS", url)
 		end
 		if buff:match("jobro") then
 			table.insert(jobro, url)
 			jobro_yes=true
+			print("JOBRO", url)
+			intr_file:write("jobro:\n"..url.."\n") intr_file:flush()
 		end	
 		if dennis_yes and jobro_yes then
 			table.insert(both, url)
+			print("BOTH", url)
+			intr_file:write("both:\n"..url.."\n") intr_file:flush()
 		end
 	end)
-	break
+--	break
 end
 
 file = io.open('lefile', 'w+')
--- lecheckurl.lua
+++ lecheckurl.lua
@@ -4,11 +4,36 @@ local check = function(url, checkfunc)
 	if tmpname then
 		local file = io.open('data/'..tmpname)
 		if not file then 
-			os.execute("curl -o data/"..tmpname.. " " .. url)
+			os.execute("curl -s -o data/"..tmpname.. " " .. url)
+		else
+			print('\texists')
 		end
 		local file = io.open('data/'..tmpname);
+	
 		local buff = file:read("*a")
-		return checkfunc(buff)
+		
+		local part = buff:match("<p>Saved <strong>.-</p>")
+		if part then
+			local matchn=0 
+			local url
+			for match in string.gmatch(part, '<a href=\"([^\"]*)"' ) do 
+				matchn=matchn+1
+			--	print(match)
+				if matchn==2 then 
+					url = 'http://web.archive.org'..match
+					local fname = "data/"..tmpname.."t"
+					local file = io.open(fname)
+					if  not file then
+					 	os.execute('curl -s -o '..fname..' ' .. url)
+					else
+						print('\t\talso exists')
+					end 
+					local f = io.open(fname)
+					local buff = f:read("*a")
+					return checkfunc(buff)
+				end
+			end
+		end
 	end
 end
 
